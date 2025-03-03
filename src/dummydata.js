9dba9afc0965ad05a5eebc95f8e6f95f71740e60
Tharun <tharungowda30115@gmail.com>
2025-03-03 22:40:47 +0530

    few changes

--- src/dummydata.js
+++ src/dummydata.js
@@ -5,3 +5,16 @@ export const homeAbout = [
       title: "Online Courses",
       desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
+    {
+        id: 1,
+        cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+        title: "Earn A Certificates",
+        desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+      },
+      {
+        id: 1,
+        cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+        title: "Learn with Expert",
+        desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+      },
+    ]
